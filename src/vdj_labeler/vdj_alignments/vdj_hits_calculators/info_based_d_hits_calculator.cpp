e4a9d7b56b31b67d0c4eee142e27a1f93515d12e
Yana Safonova <safonova.yana@gmail.com>
2015-12-06 23:21:24 +0300

    test filter selects D hit with the best score

--- src/vdj_labeler/vdj_alignments/vdj_hits_calculators/info_based_d_hits_calculator.cpp
+++ src/vdj_labeler/vdj_alignments/vdj_hits_calculators/info_based_d_hits_calculator.cpp
@@ -19,14 +19,22 @@ IgGeneSegmentHitsPtr InfoBasedDHitsCalculator::ComputeHits(ReadPtr read_ptr) {
     IgGeneAlignmentPositions v_alignment_positions = vj_alignment_info_.GetVAlignmentByIndex(read_index);
     IgGeneAlignmentPositions j_alignment_positions = vj_alignment_info_.GetJAlignmentByIndex(read_index);
     IgGeneSegmentHitsPtr d_hits_ptr(new IgGeneSegmentHits(IgGeneType::diversity_gene, read_ptr));
+    vector<IgGeneAlignmentPtr> d_alignments;
+    int max_score = 0;
     for(auto d_gene = d_gene_database_.cbegin(); d_gene != d_gene_database_.cend(); d_gene++) {
         IgGeneAlignmentPositions d_alignment_pos = ComputeDAlignmentPositions(v_alignment_positions,
                                                                               j_alignment_positions,
                                                                               *d_gene,
                                                                               read_ptr);
         auto d_alignment = d_gene_aligner_.ComputeAlignment(d_alignment_pos);
-        if(estimator_.AlignmentIsGood(d_alignment))
-            d_hits_ptr->AddHit(d_alignment);
+        d_alignments.push_back(d_alignment);
+        max_score = max<int>(max_score, d_alignment->Score());
+        //if(estimator_.AlignmentIsGood(d_alignment))
+        //    d_hits_ptr->AddHit(d_alignment);
     }
+    // just a stub
+    for(auto it = d_alignments.begin(); it != d_alignments.end(); it++)
+        if((*it)->Score() == max_score)
+            d_hits_ptr->AddHit(*it);
     return d_hits_ptr;
 }
