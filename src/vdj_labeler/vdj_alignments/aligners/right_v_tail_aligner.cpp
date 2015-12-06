e9dc13b77b6fadfec50a35ec759a2cbea9997a65
Yana Safonova <safonova.yana@gmail.com>
2015-12-07 02:54:59 +0300

    refinement of v alignment positions

--- src/vdj_labeler/vdj_alignments/aligners/right_v_tail_aligner.cpp
+++ src/vdj_labeler/vdj_alignments/aligners/right_v_tail_aligner.cpp
@@ -12,7 +12,17 @@ using namespace std;
 using namespace seqan;
 
 void RightVTailAligner::RefineAlignmentPositions(IgGeneAlignmentPtr alignment_ptr) {
-    // todo: compute alignment positions
+    // todo: debug me!
+    auto row1 = row(alignment_ptr->Alignment(), 0);
+    auto row2 = row(alignment_ptr->Alignment(), 1);
+    size_t alignment_length = length(row1);
+    size_t start_read_pos = alignment_ptr->Positions().alignment.query_pos.second + 1;
+    size_t end_read_pos = start_read_pos + toSourcePosition(row1, alignment_length - 1);
+    size_t start_gene_pos = alignment_ptr->Positions().alignment.subject_pos.second + 1;
+    size_t end_gene_pos = start_gene_pos + toSourcePosition(row2, alignment_length - 1);
+    alignment_ptr->RefineAlignmentPositions(AlignmentPositions(
+            make_pair(start_read_pos, end_read_pos),
+            make_pair(start_gene_pos, end_gene_pos)));
 }
 
 IgGeneAlignmentPtr RightVTailAligner::ComputeAlignment(IgGeneAlignmentPositions alignment_positions) {
@@ -30,5 +40,7 @@ IgGeneAlignmentPtr RightVTailAligner::ComputeAlignment(IgGeneAlignmentPositions 
     assignSource(row(align, 1), suffix(alignment_positions.ig_gene->seq(),
                                        alignment_positions.alignment.subject_pos.second));
     int score = globalAlignment(align, Score<int, Simple>(2, -1, -3, -2));
-    return IgGeneAlignmentPtr(new IgGeneAlignment(alignment_positions, align, score));
+    IgGeneAlignmentPtr v_alignment(new IgGeneAlignment(alignment_positions, align, score));
+    RefineAlignmentPositions(v_alignment);
+    return v_alignment;
 }
