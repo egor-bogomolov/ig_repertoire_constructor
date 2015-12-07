db5063bae04aedd47d13ef10eb51cbc6d4917e48
Yana Safonova <safonova.yana@gmail.com>
2015-12-07 13:35:41 +0300

    cleanup

--- src/vdj_labeler/vdj_alignments/aligners/left_j_tail_aligner.cpp
+++ src/vdj_labeler/vdj_alignments/aligners/left_j_tail_aligner.cpp
@@ -14,11 +14,11 @@ void LeftJTailAligner::RefineAlignmentPositions(IgGeneAlignmentPtr alignment_ptr
     size_t alignment_length = length(row1);
     // alignment can contain gaps =>
     // alignment length and number of read or gene nucleotides participating in alignment can be different
+    // refinement of read position
     size_t read_alignment_length = toSourcePosition(row1, alignment_length - 1) + 1;
-    cout << "read_alignment_length: " << read_alignment_length << endl;
     size_t end_read_pos = alignment_ptr->Positions().alignment.query_pos.first - 1;
     size_t start_read_pos = end_read_pos - read_alignment_length + 1;
-    // refinement gene
+    // refinement of gene position
     size_t gene_alignment_length = toSourcePosition(row2, alignment_length - 1) + 1;
     size_t end_gene_pos = alignment_ptr->Positions().alignment.subject_pos.first - 1;
     size_t start_gene_pos = end_gene_pos - gene_alignment_length + 1;
@@ -33,7 +33,6 @@ IgGeneAlignmentPtr LeftJTailAligner::ComputeAlignment(IgGeneAlignmentPositions a
     if(alignment_positions.alignment.query_pos.first == 1)
         return IgGeneAlignmentPtr(new IgGeneAlignment(alignment_positions, align, -1));
     size_t tail_length = alignment_positions.alignment.subject_pos.first;
-    //cout << "Tail length: " << tail_length << endl;
     auto read_segment = prefix(
             suffix(alignment_positions.read->seq, alignment_positions.alignment.query_pos.first -
             tail_length - left_shift_), tail_length + left_shift_);
